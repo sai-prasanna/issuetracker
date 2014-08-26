445ab8a87681aa70ffeaa627eee5c02ba6d9a6bd
Sai Prasanna <sai.r.prasanna@gmail.com>
2014-08-26 20:12:38 +0530

    Remove status from create form

--- main/forms.py
+++ main/forms.py
@@ -4,4 +4,4 @@ from main.models import Ticket
 class CreateTicketForm(forms.ModelForm):
     class Meta:
         model = Ticket
-        exclude = ('logged_by',)
+        exclude = ('logged_by', 'status',)
